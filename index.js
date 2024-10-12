bf075a56c53a977154b8a133788b019bfeecc522
wushuo <2725730722@qq.com>
2024-10-12 18:56:07 +0800

    init

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 import api from "./api.js";
+import {getBgmId, save} from "./bgm.js";
 
+const cheerio = await import('cheerio');
 
-(async () => {
-    let res = await api.get(`https://api.bgm.tv/v0/subjects/475354`)
-    console.log(res);
-})()
+let mikanHost = 'https://mikanani.me'
+let res = await fetch(mikanHost)
+let html = await res.text()
+
+const $ = cheerio.load(html);
+for (let a of Array.from($('a'))) {
+    let href = a.attribs['href']
+    if (!href instanceof String) {
+        continue
+    }
+    if (!href) {
+        continue
+    }
+    let regex = /^\/Home\/Bangumi\/\d+$/
+    if (!href.match(regex)) {
+        continue
+    }
+    await sleep(1000)
+    let bgmId = await getBgmId(mikanHost + href)
+    await save(bgmId)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+res = await api.get(`https://api.bgm.tv/v0/subjects/475354`)
